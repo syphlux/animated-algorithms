e95861a7f63cb96badc858e84fe849f1950c1d75
Syphax Ait oubelli <syphaxp@gmail.com>
2025-05-21 12:11:36 +0100

    animated reusable array

---
+++ data_structures/1d_array.py
@@ -0,0 +1,108 @@
+from manim import *
+
+class ArrayElement(VGroup):
+    def __init__(self, value, index=0):
+        super().__init__()
+        self.value = value
+        self.box = Rectangle(height=1, width=1)
+        self.text = Text(str(value), font_size=32)
+        self.index = Text(str(index), font='Consolas', font_size=24)
+        self._bring_text_and_index()
+        self.add(self.box, self.text, self.index)
+
+    def _bring_text_and_index(self):
+        self.text.move_to(self.box)
+        self.index.next_to(self.box, DOWN)
+        
+
+class Array(VGroup):
+    def __init__(
+            self,
+            values,
+            name='',
+            array_center=ORIGIN
+    ):
+        super().__init__()
+        self.values: list = values
+        self.n = len(values)
+        self.name = name
+        self.array_center = array_center
+        
+        self.elems = VGroup([ArrayElement(v, i) for i, v in enumerate(values)]).arrange(RIGHT, buff=0.0).move_to(array_center)
+        self.add(self.elems)
+
+    def display(self, scene: Scene):
+        scene.play(Create(self))
+        return self
+        
+    def insert(self, value, index=0):
+        pass
+        
+    def append(self, value, scene: Scene|None=None):
+        self.values.append(value)
+        self.n += 1
+        elem = ArrayElement(value, self.n-1).next_to(self.elems, RIGHT, buff=1.0)
+        if scene:
+            scene.play(Create(elem), run_time=0.25)
+            scene.play(elem.animate.next_to(self.elems, RIGHT, buff=0.0), run_time=0.25)
+        self.elems.add(elem)
+        if scene:
+            scene.play(self.animate.move_to(self.array_center), run_time=0.25)
+
+    def pop(self, scene: Scene|None=None):
+        if self.n > 0:
+            self.values.pop()
+            self.n -= 1
+            elem = self.elems[-1]
+            self.elems.remove(elem)
+            if scene:
+                scene.play(Uncreate(elem), run_time=0.25)
+                scene.play(self.animate.move_to(self.array_center), run_time=0.25)
+
+    def switch(self, i: int, j: int, scene: Scene|None=None):
+        if 0 <= i < self.n and 0 <= j < self.n and i != j:
+            self.values[i], self.values[j] = self.values[j], self.values[i]
+            if scene:
+                scene.play(
+                    self.elems[i].text.animate.shift(UP*2),
+                    self.elems[j].text.animate.shift(UP*2),
+                    run_time=0.25
+                )
+                scene.play(
+                    self.elems[i].text.animate.move_to(self.elems[j].text),
+                    self.elems[j].text.animate.move_to(self.elems[i].text),
+                    run_time=0.25
+                )
+                scene.play(
+                    self.elems[i].text.animate.shift(DOWN*2),
+                    self.elems[j].text.animate.shift(DOWN*2),
+                    run_time=0.25
+                )
+            self.elems[i].text, self.elems[j].text = self.elems[j].text, self.elems[i].text
+
+    
+
+
+        
+        
+
+
+
+
+class TestManim(Scene):
+    def construct(self):
+        a = Array([20, 35, 1, 8, 100, 2, 5]).display(self)
+        self.wait(1)
+        a.pop(self)
+        a.append(0, self)
+        a.pop(self)
+        a.switch(1, 4, self)
+        a.append(50, self)
+        a.switch(1, 4, self)
+        a.switch(1, 6, self)
+        self.wait(1)
+
+
+
+
+        
