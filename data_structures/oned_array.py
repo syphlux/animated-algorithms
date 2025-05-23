05ef15fd52f6bf6d2b2dabd6b185890546518e34
Syphax Ait oubelli <syphax.aitoubelli@gmail.com>
2025-05-23 15:53:57 +0100

    renamed 1d array file

--- data_structures/oned_array.py
+++ data_structures/oned_array.py
@@ -59,22 +59,36 @@ class Array(VGroup):
             scene.play(Create(new_elem), run_time=0.25)
             scene.play(
                 new_elem.animate.move_to(self.elems[idx]),
-                *[elem.animate.shift(RIGHT * elem.width) for elem in self.elems[idx:]],
-                *[Transform(elem.index, Text(str(int(elem.index.text)+1), font='Consolas', font_size=24).move_to(elem.index).shift(RIGHT*elem.width)) for elem in self.elems[idx:]]
+                *[elem.animate.shift(RIGHT*elem.width) for elem in self.elems[idx:]],
+                *[Transform(elem.index, Text(
+                    str(int(elem.index.text)+1), font='Consolas', font_size=24).move_to(elem.index).shift(RIGHT*elem.width)
+                ) for elem in self.elems[idx:]]
             )
             for elem in self.elems[idx:]:
                 elem.index.text = str(int(elem.index.text)+1)
             self.elems.insert(idx, new_elem)
             scene.play(self.animate.move_to(self.array_center), run_time=0.25)
                 
-    def pop(self, scene: Scene):
-        if self.n > 0:
-            self.values.pop()
-            self.n -= 1
-            elem = self.elems[-1]
-            self.elems.remove(elem)
-            scene.play(Uncreate(elem), run_time=0.25)
-            scene.play(self.animate.move_to(self.array_center), run_time=0.25)
+    def pop(self, scene: Scene, idx: int=-1):
+        if idx >= self.n or idx < -self.n:
+            return
+        elif -self.n <= idx < 0:
+            idx += self.n
+        self.values.pop(idx)
+        self.n -= 1
+        old_elem = self.elems[idx]
+        scene.play(old_elem.animate.shift(UP*2), run_time=0.25)
+        scene.play(
+            Uncreate(old_elem),
+            *[elem.animate.shift(LEFT*elem.width) for elem in self.elems[idx+1:]],
+            *[Transform(elem.index, Text(
+                str(int(elem.index.text)-1), font='Consolas', font_size=24).move_to(elem.index).shift(LEFT*elem.width)
+            ) for elem in self.elems[idx+1:]]
+        )
+        for elem in self.elems[idx+1:]:
+            elem.index.text = str(int(elem.index.text)-1)
+        self.elems.remove(old_elem)
+        scene.play(self.animate.move_to(self.array_center), run_time=0.25)
 
     def switch(self, scene: Scene, i: int, j: int):
         if 0 <= i < self.n and 0 <= j < self.n and i != j:
@@ -99,12 +113,13 @@ class Array(VGroup):
     def _reorder(self, scene: Scene, indices: list[int]):
         self.values = [self.values[idx] for idx in indices]
         scene.play(
-            *[self.elems[idx].text.animate.move_to(self.elems[i].text) for i, idx in enumerate(indices)],
-            *[self.elems[idx].box.animate.move_to(self.elems[i].box) for i, idx in enumerate(indices)]
+            *[self.elems[idx].animate.move_to(self.elems[i]) for i, idx in enumerate(indices)]
         )
-        scene.clear()
-        self.elems = VGroup([ArrayElement(v, i) for i, v in enumerate(self.values)]).arrange(RIGHT, buff=0.0).move_to(self.array_center)
-        scene.add(self.elems)
+        scene.play(*[Transform(self.elems[idx].index, Text(str(i), font='Consolas', font_size=24).move_to(self.elems[idx].index)) for i, idx in enumerate(indices)])
+        for i, idx in enumerate(indices):
+            self.elems[idx].index.text = str(i)
+        self.elems.submobjects.sort(key=lambda elem: int(elem.index.text))
+
         
     def sort(self, scene: Scene, reverse: bool=False):
         indices = sorted(list(range(len(self.values))), key=lambda i: self.values[i], reverse=reverse)
@@ -117,30 +132,64 @@ class Array(VGroup):
 
 class TestManim(Scene):
     def construct(self):
-        a = Array([20, 35, 1, 8, 100, 2, 5]).display(self)
-        self.wait(1)
-        a.insert(self, 2, 400)
-        self.wait(1)
-        a.insert(self, 0, 99)
-        self.wait(1)
-        a.insert(self, 9, 555)
-        self.wait(1)
-        a.insert(self, 9, 666)
-        self.wait(1)
-        a.insert(self, 0, 333)
-        self.wait(1)
-        a.insert(self, 3, -9)
-        self.wait(1)
-        a.append(self, 88)
-        self.wait(1)
+        a = Array([20, 35, 1, 8, 100, 2, 5, 4, 80]).display(self)
+        # self.wait(1)
+        # a.insert(self, 2, 400)
+        # self.wait(1)
+        # a.insert(self, 0, 99)
+        # self.wait(1)
+        # a.insert(self, 9, 555)
+        # self.wait(1)
+        # a.insert(self, 9, 666)
+        # self.wait(1)
+        # a.insert(self, 0, 333)
+        # self.wait(1)
+        # a.pop(self)
+        # self.wait()
+        # a.pop(self, 3)
+        # self.wait()
+        # a.insert(self, 3, -9)
+        # self.wait(1)
+        # a.pop(self, 0)
+        # self.wait()
+        # a.append(self, 88)
+        # self.wait(1)
+        a.sort(self)
+        self.wait()
+        a.sort(self, True)
+        self.wait()
+        a.insert(self, 4, 99)
+        self.wait()
+        a.sort(self)
+        self.wait()
+        a.pop(self)
+        self.wait()
         a.shuffle(self)
-        self.wait(1)
-        a.sort(self)
-        self.wait(1)
-        a.switch(self, 1, 4)
-        self.wait(1)
-        a.sort(self)
-        self.wait(1)
+        self.wait()
+        # self.wait(1)
+        # a.pop(self, 1)
+        # self.wait()
+        # a.sort(self)
+        # self.wait(1)
+        # a.switch(self, 1, 4)
+        # self.wait(1)
+        # a.sort(self)
+        # self.wait(1)
+        # a.switch(self, 0, 3)
+        # self.wait(1)
+        # a.sort(self, True)
+        # self.wait(1)
+        # a.append(self, 77)
+        # self.wait()
+        # a.shuffle(self)
+        # self.wait()
+        # a.pop(self, 0)
+        # self.wait()
+        # a.pop(self)
+        # self.wait()
+        # a.sort(self)
+        # self.wait()
+
         
 
 
