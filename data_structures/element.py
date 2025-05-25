5431c31f28f2dbb5f386e149a2849b3cf22bdcee
Syphax Ait oubelli <syphax.aitoubelli@gmail.com>
2025-05-25 09:43:59 +0100

    added highlight() and replace_value() to Element

--- data_structures/element.py
+++ data_structures/element.py
@@ -20,6 +20,7 @@ class Element(VGroup):
         super().__init__()
         self.value = value
         self.box = box_style.copy()
+        self.content_style = content_style
         self.content = Text(str(value), **content_style).move_to(self.box.get_center() + content_direction)
         self.content.scale_to_fit_width(min(self.content.width, self.box.width*0.8))
         self.label = Text(str(label), **label_style)
@@ -27,18 +28,43 @@ class Element(VGroup):
         self.label.next_to(self.box, label_direction, label_buff)
         self.add([self.box, self.content] + ([self.label] if label is not None else []))
 
+    def replace_value(self, scene: Scene, new_value: any, run_time: float=0.5):
+        self.value = new_value
+        self.content.text = str(new_value)
+        scene.play(Transform(self.content, Text(
+            str(new_value), **self.content_style).move_to(self.content)
+        ), run_time=run_time)
+
+    def highlight(
+            self,
+            scene: Scene,
+            color: ParsableManimColor=YELLOW, 
+            font_color: ParsableManimColor=BLACK, 
+            scale_ratio: float=1.2, 
+            run_time: float=0.5
+    ):
+        self.save_state()
+        scene.play(
+            self.box.animate.set_stroke(color=color, opacity=1.0).set_color(color).set_opacity(0.8).scale(scale_ratio),
+            self.content.animate.set_color(font_color),
+            self.label.animate.scale(scale_ratio),
+            run_time=run_time/3
+        )
+        scene.wait(run_time/3)
+        scene.play(Restore(self), run_time=run_time/3)
+
 
 class TestElement(Scene):
     def construct(self):
-        top = Ellipse(height=0.3)
-        bot = Arc(radius=1, angle=-PI).shift(DOWN*0.5).stretch_to_fit_height(0.15)
-
-        left_edge = Line(top.get_left(), bot.get_corner(UL))
-        right_edge = Line(top.get_right(), bot.get_corner(UR))
-        puce = VGroup(top, bot, left_edge, right_edge).set_stroke(color=WHITE).set_color(BLACK).set_opacity(1.0)
-        self.add(puce)
-        elem = Element(45, label_direction=UP, box_style=puce, content_direction=DOWN*0.15).shift(UP*2)
-        elem2 = elem.copy().next_to(elem, UP, buff=-0.3)
-        self.add(elem, elem2)
+        elem = Element(45, 'max')
+        self.play(Create(elem))
+        self.wait(0.5)
+        elem.highlight(self)
+        self.wait()
+        elem.replace_value(self, 98)
+        print(elem.content.text)
+        self.wait()
+        self.play(elem.animate.shift(RIGHT))
+        self.wait()
 
         
