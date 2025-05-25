18e50f8c9814600adfaf95ebc29ae031a86d8b98
Syphax Ait oubelli <syphax.aitoubelli@gmail.com>
2025-05-25 10:21:48 +0100

    Update pointer.py

--- data_structures/pointer.py
+++ data_structures/pointer.py
@@ -1,36 +1,51 @@
 from manim import *
+import numpy.typing as npt
+from typing_extensions import TypeAlias
 
+
+Vector3D: TypeAlias = npt.NDArray[np.float64]
 
 class Pointer(VGroup):
-    def __init__(self, shape: VMobject, label: str=''):
+    def __init__(
+            self, 
+            pointer_style: VMobject=Circle(radius=0.2, color=ORANGE, fill_opacity=1.0), 
+            label: any=None,
+            label_buff: float=DEFAULT_MOBJECT_TO_MOBJECT_BUFFER,
+            label_style: dict={'font': 'Consolas', 'font_size': 24}
+        ):
         super().__init__()
-        self.shape = shape
-        self.label = Text(label, font='Consolas', font_size=24)
+        self.shape = pointer_style.copy()
+        self.label_buff = label_buff
+        self.label = Text(label, **label_style).next_to(self.shape, buff=label_buff)
         self.angle = 0
-        self.add(shape)
-        if label:
-            self.add(self.label)
-
-    def _find_best_direction(self, pointed_at: VMobject, points_to_avoid: list[VMobject], num_dirs: int=36):
-        angles = np.linspace(0, 2*np.pi, num_dirs)
+        self.add([self.shape] + ([self.label] if label is not None else []))
+        
+    def _find_best_direction(self, pointed_at: Mobject, to_avoid: list[Vector3D|Mobject], num_dirs: int=36):
+        angles = np.linspace(0, TAU, num_dirs)
         potential_directions = [np.array([np.cos(a), np.sin(a), 0]) for a in angles]
-        center = pointed_at.get_center()
-        other_centers = [p.get_center() for p in points_to_avoid]
+        centers_to_avoid = [p if isinstance(p, Vector3D) else p.get_center() for p in to_avoid]
+        # returns the direction with the greatest total sum of distances from points to avoid
         return max(
             potential_directions, 
-            key=lambda d: sum([np.linalg.norm(oc - (center + d)) for oc in other_centers])
+            key=lambda d: sum([np.linalg.norm(c - (pointed_at.get_center() + d)) for c in centers_to_avoid])
         )
 
-    def point_at(self, scene: Scene, pointed_at, direction=DOWN, buff=0.5, points_to_avoid=None, run_time=0.5):
+    def point_at(
+            self, 
+            scene: Scene, 
+            pointed_at: Mobject, 
+            direction: Vector3D|str=DOWN, 
+            buff: float=DEFAULT_MOBJECT_TO_MOBJECT_BUFFER, 
+            to_avoid: list[Vector3D|Mobject]=None, 
+            run_time: float=0.5
+        ):
         if isinstance(direction, str) and direction == 'auto':
-            if not isinstance(points_to_avoid, list) or points_to_avoid == []:
-                direction = DOWN
-            else:
-                direction = self._find_best_direction(pointed_at, points_to_avoid)
-        rotation_degrees = 90+np.degrees(np.arctan2(direction[1], direction[0]))
+            direction = self._find_best_direction(pointed_at, to_avoid) if to_avoid != [] else DOWN
+        rotation_degrees = 90 + np.degrees(np.arctan2(direction[1], direction[0]))
+        target_shape = self.shape.copy().next_to(pointed_at, direction, buff).rotate((rotation_degrees-self.angle)*DEGREES)
         scene.play(
             self.shape.animate.next_to(pointed_at, direction, buff).rotate((rotation_degrees-self.angle)*DEGREES),
-            self.label.animate.next_to(pointed_at, direction*2, buff+0.25),
+            self.label.animate.next_to(target_shape, direction, self.label_buff),
             run_time=run_time
         )
         self.angle = rotation_degrees
@@ -39,18 +54,16 @@ class Pointer(VGroup):
 class TestPointer(Scene):
     def construct(self):
         d = Square(z_index=float('inf')).shift(UL)
-        p = Pointer(Triangle(fill_opacity=1.0).scale(0.2), 'curr')
+        p = Pointer(label='curr')
         self.play(Create(d))
-        p.point_at(self, d, UP, buff=0.5)
+        p.point_at(self, d, UP, buff=0.0)
         self.wait(0.5)
-        p.point_at(self, d, DOWN, buff=0.5)
+        p.point_at(self, d, DOWN, buff=0.0)
         self.wait(0.5)
-        p.point_at(self, d, LEFT, buff=0.5)
+        p.point_at(self, d, LEFT, buff=0.0)
         self.wait(0.5)
-        p.point_at(self, d, UR, buff=0.5)
+        p.point_at(self, d, UR, buff=0.0)
         self.wait(0.5)
-        p.point_at(self, d, DL, buff=0.5)
+        p.point_at(self, d, DL, buff=0.0)
         self.wait(0.5)
                 
-        
-        
