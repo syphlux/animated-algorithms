726ee06ef33c1e7155668b708abb9fa7ce097470
Syphax Ait oubelli <syphax.aitoubelli@gmail.com>
2025-06-04 16:43:39 +0100

    minor changes to StackElement

--- data_structures/stack.py
+++ data_structures/stack.py
@@ -9,7 +9,7 @@ Vector3D: TypeAlias = npt.NDArray[np.float64]
 class StackElement(VGroup):
     def __init__(
             self, 
-            height: float=1.3, 
+            height: float=1.3, # total height (ellipse height + body height)
             width: float=2.0, 
             ellipse_height: float=0.3,
             stroke_color: ParsableManimColor=WHITE,
@@ -18,18 +18,18 @@ class StackElement(VGroup):
         ):
         self.body_height = height - ellipse_height
         self.ellipse_height = ellipse_height
-        arc = Arc(radius=width/2, angle=-PI).set_fill(
+        bottom_arc = Arc(radius=width/2, angle=-PI).set_fill(
             color=fill_color, opacity=fill_opacity
         ).stretch_to_fit_height(ellipse_height/2)
         left_edge = Line(
-            arc.get_left() + ellipse_height/5*UP, arc.get_left() + (ellipse_height/5+self.body_height)*UP,
+            bottom_arc.get_left() + ellipse_height/5*UP, bottom_arc.get_left() + (ellipse_height/5+self.body_height)*UP,
             color=stroke_color
         )
         right_edge = left_edge.copy().shift(width*RIGHT)
+        top_ellipse = Ellipse(width=width, height=ellipse_height).set_fill(fill_color, fill_opacity).set_stroke(stroke_color).move_to(left_edge.get_end() + width/2 * RIGHT)
         rectangle = Rectangle(height=self.body_height, width=width).next_to(left_edge, buff=0.0)
-        ellipse = Ellipse(width=width, height=ellipse_height).set_fill(fill_color, fill_opacity).set_stroke(stroke_color).move_to(left_edge.get_end() + width/2 * RIGHT)
-        rectangle = Difference(Difference(rectangle, ellipse), arc).set_fill(fill_color, fill_opacity).set_stroke(None, width=0.0, opacity=0.0)
-        super().__init__(arc, left_edge, right_edge, rectangle, ellipse)
+        rectangle = Difference(Difference(rectangle, top_ellipse), bottom_arc).set_fill(fill_color, fill_opacity).set_stroke(None, width=0.0, opacity=0.0)
+        super().__init__(bottom_arc, left_edge, right_edge, rectangle, top_ellipse)
         self.center()
 
 
